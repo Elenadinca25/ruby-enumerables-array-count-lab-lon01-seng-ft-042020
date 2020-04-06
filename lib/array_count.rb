5ad5159d955eecc8ab02fe6e5bf07a6a148d0fbb
Elena Dinca <elenadinca25@gmail.com>
2020-04-06 17:53:18 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,5 +1,6 @@
 def count_strings(array)
-  array.grep(String).count
+  #array.grep(String).count
+  array.map{|string| string.count}
 end
 # Return the total number of strings in the provided array using the count enumerable
 
@@ -8,5 +9,6 @@ end
 
 def count_empty_strings(array)
   array.count("")
+
 end
 # Return the total number of EMPTY strings in the provided array using the count enumerable
